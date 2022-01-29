a90a4d2417ffd3949b522201b9bb88d0afe79fd4
Aarav-classes <83007670+Aarav-classes@users.noreply.github.com>
2022-01-29 15:39:03 +0530

    Add files via upload

---
+++ AI music-web-app/main.js
@@ -0,0 +1,11 @@
+rightWristY=0;
+leftWristY=0;
+scoreRightWrist=0;
+scoreLefttWrist=0;
+song="";
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.position(500,200);
+    video=createCapture(VIDEO);
+    video.hide();
+}
